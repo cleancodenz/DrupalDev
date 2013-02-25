1b450192791d5689b0809921026c978158581b36
cleancodenz2 <cleancodenz2@cleancode.co.nz>
2013-02-25 15:22:34 +1300

    bootstrap themes modified

--- sites/all/themes/bootstrapcc/template.php
+++ sites/all/themes/bootstrapcc/template.php
@@ -1,5 +1,6 @@
 <?php
 
 function bootstrapcc_js_alter(&$javascript) {
-  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'bootstrapcc').'/js/jquery.js';
+  
+  $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'bootstrapcc').'/jquery/jquery-1.9.1.js';
 }
