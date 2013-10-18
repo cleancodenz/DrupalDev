dfad5d8c201618bf4c053a1ec0081602905bb0cb
cleancodenz2 <cleancodenz2@cleancode.co.nz>
2013-10-18 16:29:07 +1300

    dropdown menu dev

--- sites/all/modules/ccpuriri/modules/ccpuriri_common/ccpuriri_common_dropdownbutton.js
+++ sites/all/modules/ccpuriri/modules/ccpuriri_common/ccpuriri_common_dropdownbutton.js
@@ -1,4 +1,31 @@
 (function($) {
 
-	
+	// declare a global object which is initialized by attach
+	Drupal.ccpuriri = Drupal.ccpuriri || {};
+	Drupal.ccpuriri.dropdownbutton = Drupal.ccpuriri.dropdownbutton || {};
+
+	Drupal.behaviors.DROPDOWNBUTTON = {
+		attach : function(context, settings) {
+			if (settings.dropdownbuttonids){
+				 for (var base in settings.dropdownbuttonids) {
+						// binding the select event to every li select
+					$('#'+settings.dropdownbuttonids[base]+'-div > ul.dropdown-menu > li ').bind('click',function(){
+						// set teh value to hidden input
+						$('#'+settings.dropdownbuttonids[base]+'-div > input[type="hidden"]').val($(this).val());
+						
+						// trigger the ajax event
+						$('#'+settings.dropdownbuttonids[base]).trigger('itemselected');
+						
+					});
+						
+					 
+				 }
+				
+			}
+			
+			
+		},
+
+	};
+
 })(jQuery);
